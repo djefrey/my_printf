f78651ae108c59524544b2e53aaabdfd6cc2f304
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-11-03 16:19:49 +0100

    more tests

--- tests.c
+++ tests.c
@@ -27,7 +27,7 @@ Test(my_printf, simple_str_newline, .init = redirect_all_std)
     cr_assert_stdout_eq_str("Hello\nWorld");
 }
 
-Test(my_printf, print_ints, .init = redirect_all_std)
+Test(my_printf, print_int, .init = redirect_all_std)
 {
     my_printf("I'm %i", 18);
     cr_assert_stdout_eq_str("I'm 18");
@@ -35,8 +35,8 @@ Test(my_printf, print_ints, .init = redirect_all_std)
 
 Test(my_printf, print_ints, .init = redirect_all_std)
 {
-    my_printf("I'm %i ans my sister is %i", 18, 17);
-    cr_assert_stdout_eq_str("I'm 18");
+    my_printf("I'm %i and my sister is %i", 18, 17);
+    cr_assert_stdout_eq_str("I'm 18 and my sister is 17");
 }
 
 Test(my_printf, print_char, .init = redirect_all_std)
@@ -53,12 +53,40 @@ Test(my_printf, print_chars, .init = redirect_all_std)
 
 Test(my_printf, print_str, .init = redirect_all_std)
 {
-    my_printf("Hey %s !", "slt");
+    my_printf("Hey, %s !", "slt");
     cr_assert_stdout_eq_str("Hey, slt !");
 }
 
 Test(my_printf, print_strs, .init = redirect_all_std)
 {
-    my_printf("Hey, %c%c%c !", 's', 'l', 't');
-    cr_assert_stdout_eq_str("Hey, slt !");
+    my_printf("%s à tous, je %s dire donc %s %s", "Bonjour", "sais pas quoi", "je", "rempli");
+    cr_assert_stdout_eq_str("Bonjour à tous, je sais pas quoi dire donc je rempli");
+}
+
+Test(my_printf, print_fullstr, .init = redirect_all_std)
+{
+    char str[] = "42";
+
+    str[0] = 4;
+    my_printf("Le nombre est %S", str);
+    cr_assert_stdout_eq_str("Le nombre est \\0042");
+}
+
+Test(my_printf, print_fullstrduo, .init = redirect_all_std)
+{
+    char str[] = "42";
+
+    str[0] = 4;
+    str[1] = 2;
+    my_printf("Le nombre est %S", str);
+    cr_assert_stdout_eq_str("Le nombre est \\004\\002");
+}
+
+Test(my_printf, print_fullstr_len2, .init = redirect_all_std)
+{
+    char str[] = " ";
+
+    str[0] = 15;
+    my_printf("%S", str);
+    cr_assert_stdout_eq_str("\\015");
 }
