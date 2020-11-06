94ba9a102d4a852b09899decf72b67958a8ce9ce
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-11-06 15:11:29 +0100

    fix too long function

--- my_printf/my_printf.c
+++ my_printf/my_printf.c
@@ -9,15 +9,29 @@
 #include "my_printf.h"
 #include "my.h"
 
+static int print_percent(char **str)
+{
+    my_putchar('%');
+    *str += 1;
+    return (1);
+}
+
+static void print_specifier(int i, char **str, va_list *list,
+va_list_fct_t length_modif)
+{
+    void *value = VA_LIST_SPEC[i] != NULL ?
+    VA_LIST_SPEC[i](list) : (*length_modif)(list);
+
+    PRINT_FCTS[i](value);
+    if (i != FLAG_PTR_ID)
+        free(value);
+    *str += 1;
+}
+
 static int check_flag(char **str, va_list *list)
 {
     va_list_fct_t length_modif = &va_list_nbint;
 
-    if (**str == '%') {
-        my_putchar('%');
-        *str += 1;
-        return (1);
-    }
     for (int i = 0; i < NUMBER_MODIFIERS; i++) {
         if (!my_strcmp(*str, MODIFIERS[i])) {
             length_modif = VA_LIST_MOD[i];
@@ -25,14 +39,11 @@ static int check_flag(char **str, va_list *list)
             break;
         }
     }
+    if (**str == '%')
+        return (print_percent(str));
     for (int i = 0; i < NUMBER_SPECIFIERS; i++) {
         if (**str == SPECIFIERS[i]) {
-            void *value = VA_LIST_SPEC[i] != NULL ?
-            VA_LIST_SPEC[i](list) : (*length_modif)(list);
-            PRINT_FCTS[i](value);
-            if (i != FLAG_PTR_ID)
-                free(value);
-            *str += 1;
+            print_specifier(i, str, list, length_modif);
             return (1);
         }
     }
